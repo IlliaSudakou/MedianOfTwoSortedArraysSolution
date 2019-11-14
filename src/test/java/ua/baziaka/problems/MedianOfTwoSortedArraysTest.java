d158e874e16038db3c215a3caac40688d5b320a4
Illia Sudakou <illia_sudakou@epam.com>
2019-11-14 14:42:41 +0300

    tests

--- src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
+++ src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
@@ -3,14 +3,26 @@ package ua.baziaka.problems;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MedianOfTwoSortedArraysTest {
-
-    MedianOfTwoSortedArrays toTestAlg = new MedianOfTwoSortedArrays();
+    @Test
+    public void test1() {
+        assertTrue(true);
+    }
 
     @Test
-    public void testMedianOfTwoSortedArrays() {
-        double actual = toTestAlg.findMedianSortedArrays(new int[]{1, 3}, new int[]{2});
-        assertEquals(2.0d, actual);
+    public void test2() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void test3() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void test4() {
+        assertTrue(false);
     }
 }
